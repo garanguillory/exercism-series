20eb73a8c52c14007c5f6788c774178b7ebdb3e9
Garan Guillory <garanguillory@gmail.com>
2016-02-18 22:56:44 -0700

    All tests passing

--- series.js
+++ series.js
@@ -1,21 +1,19 @@
 var Series = function(input){
 
-	var array = input.split('').
+	this.digits = input.split('').
 				map(function(element){return Number(element)});
 
-
-	this.digits = array;
-
-// need to push number amount of elements to an array
-// within the original array
-// result will be an array of arrays with number being 
-// the number of elements in each array within the array
 	this.slices = function(number){
+		var array = this.digits;
 		var numbers = [];
 		var length = array.length;
-			for(var i=0; i<length; i++){
-					numbers.push(array.slice(0,number));
-					array.shift();
+			if(number > length){
+					throw 'Slice size is too big.';
+			} else {
+				for(var i=0; i<length-number+1; i++){
+						numbers.push(array.slice(0,number));
+						array.shift();
+				}
 			}
 		return numbers;
 	};
